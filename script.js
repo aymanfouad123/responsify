8e8e0aa527d6be9ec94c2ccf8a510d77506b00a5
aymanfouad123 <aymanfouad033@gmail.com>
2025-05-06 16:27:07 -0400

    feat: adding client side song fetch (learning purposes)

--- script.js
+++ script.js
@@ -1,2 +1,23 @@
 console.log("Testing script");
 
+async function getSongs(){
+    let a =  await fetch("http://127.0.0.1:5500/songs/")
+    let response = await a.text()
+    console.log(response)
+
+    let div = document.createElement("div")
+    div.innerHTML = response;
+
+    let as = div.getElementsByTagName("a")
+    let songs = []
+    for (let index = 0; index < as.length; index++) {
+        const element = as[index];
+        if (element.href.endsWith(".mp3")){
+            songs.push(element.href)
+        }
+    }
+    return songs
+}
+
+let songs = getSongs()
+console.log(songs)
